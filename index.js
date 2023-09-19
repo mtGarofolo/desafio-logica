c75e13ef754b32c1699939a90dd3857bd1197b31
mtGarofolo <theushreis@gmail.com>
2023-09-19 20:56:04 -0300

    Projeto desafio de lógica DIO

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Olá krl");
+let nickname = "Behavior"
+let xp = "5000"
+
+if (xp <= 1000) {
+
+    console.log("O herói de nome" + " " + nickname + " " + "está no nível Ferro")
+
+} else if (xp <= 2000) {
+
+    console.log("O herói de nome" + " " + nickname + " " + "está no nível Bronze")
+
+} else if (xp >= 2001 && xp <= 5000) {
+
+    console.log("O herói de nome" + " " + nickname + " " + "está no nível Prata")
+
+} else if (xp >= 6001 && xp <= 7000) {
+
+    console.log("O herói de nome" + " " + nickname + " " + "está no nível Ouro")
+
+} else if (xp >= 7001 && xp<= 8000) {
+    
+    console.log("O herói de nome" + " " + nickname + " " + "está no nível Platina")
+
+} else if (xp >= 8001 && xp<= 9000) {
+    
+    console.log("O herói de nome" + " " + nickname + " " + "está no nível Ascendente")
+
+} else if (xp >= 9001 && xp<= 10000) {
+    
+    console.log("O herói de nome" + " " + nickname + " " + "está no nível Imortal")
+} else if (xp >= 10001) {
+    
+    console.log("O herói de nome" + " " + nickname + " " + "está no nível Radiante")
+}
